26536922ed25eb2db3e34eb873a709375e9cdb0d
sradhkrish <rsenthil75@gmail.com>
2023-07-22 15:55:00 -0500

    Initial commit

---
+++ src/com/tsm/java/workshop/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.tsm.java.workshop;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int num1,num2,sum;
+        num1=10;
+        num2=15;
+        sum=num1+num2;
+        System.out.println("Sum of two numbers="+sum);
+    }
+}
---
+++ src/com/tsm/java/workshop/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.tsm.java.workshop;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World - TSM");
+    }
+}
