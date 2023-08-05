3701f862acd413ac31fce1d37949df8ce777d6d6
sradhkrish <rsenthil75@gmail.com>
2023-08-05 10:12:02 -0500

    Adding homework for reference.

---
+++ src/com/tsm/java/workshop/homework1/AddTwoNumbersPrintWithVariables.java
@@ -0,0 +1,11 @@
+package com.tsm.java.workshop.homework1;
+
+public class AddTwoNumbersPrintWithVariables {
+    public static void main(String[] args) {
+        int num1,num2,sum;
+        num1=10;
+        num2=15;
+        sum=num1+num2;
+        System.out.println("Sum of "+num1+" and "+num2+"="+sum);
+    }
+}
---
+++ src/com/tsm/java/workshop/homework1/SubtractTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.tsm.java.workshop.homework1;
+
+public class SubtractTwoNumbers {
+    public static void main(String[] args) {
+        int num1,num2,diff;
+        num1=25;
+        num2=10;
+        diff=num1-num2;
+        System.out.println("Result="+diff);
+    }
+}
